9bf2ad41a97aef7ae0f39ad47d89f33f993f4736
Sowndharya S <sowndharya.selladurai@syncfusion.com>
2025-06-03 11:17:25 +0530

    Sample added

---
+++ DockLayout-Getting-Started/DockLayoutGettingStarted/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DockLayoutGettingStarted
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
