7042c14584c585b008c35a2d1baa6bf8227290cb
Kamalesh-Periyasamy <Kamalesh.periyasamy@syncfusion.com>
2026-03-26 18:34:41 +0530

    added new sample

---
+++ DockLayout-Getting-Started/DockLayoutGettingStarted/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Syncfusion.Maui.Core;
+
+namespace DockLayoutGettingStarted
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
